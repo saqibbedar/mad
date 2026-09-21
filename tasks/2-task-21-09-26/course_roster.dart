34d3bf22826031de8745a3f8517f0817cfc2d2dc
Saqib Bedar <124094939+saqibbedar@users.noreply.github.com>
2026-09-21 11:24:48 +0500

    fix(task2/part1): follow documented rules

--- tasks/2-task-21-09-26/course_roster.dart
+++ tasks/2-task-21-09-26/course_roster.dart
@@ -1,9 +1,13 @@
+/// Course Roster Console App - Lab 1
+
 void main() {
     // Part 1: Setup & Welcome
     printWelcome("Course Roster Manager");
+
+    
 }
 
-// welcome function
+/// Prints a welcome message for application
 void printWelcome(String appName) {
-    print("$appName");
+    print("=== $appName ===");
 }
